47f955e49ef6b5d542c3aa884ef1b47fa9d4e5ad
Awesomedonut <rainyjsong@gmail.com>
2025-06-03 15:01:46 -0700

    idk if its working but ill revist this later :D

--- testDj/testDj/settings.py
+++ testDj/testDj/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'tailwind',
     'theme',
-    'django_browser_reload',
+  #  'django_browser_reload',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +46,7 @@ TAILWIND_APP_NAME = 'theme'
 
 
 MIDDLEWARE = [
+#    "django_browser_reload.middleware.BrowserReloadMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
